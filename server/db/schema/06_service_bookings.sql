d05314f5d9fece36574152540e8095afed78a7e6
Pouya Elishaee <p.elishaee@gmail.com>
2022-03-22 16:39:08 -0400

    change database tables and data

--- server/db/schema/06_service_bookings.sql
+++ server/db/schema/06_service_bookings.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS appointments CASCADE;
-CREATE TABLE appointments(
+DROP TABLE IF EXISTS service_bookings CASCADE;
+CREATE TABLE service_bookings(
     id SERIAL PRIMARY KEY NOT NULL,
     title VARCHAR(255),
     rating INT,
@@ -7,6 +7,9 @@ CREATE TABLE appointments(
     created_at TIMESTAMPTZ DEFAULT Now() ,
     st_date timestamptz,
     end_date timestamptz,
+    accepted_at TIMESTAMPTZ DEFAULT NULL,
+    rejected_at TIMESTAMPTZ DEFAULT NULL,
+    finished_at TIMESTAMPTZ DEFAULT NULL,
     services_id INTEGER REFERENCES services(id) ON DELETE CASCADE,
     users_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 
