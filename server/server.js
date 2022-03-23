056bcd2ec97b9e4bbf6a3f05df1d22290701e4f0
ibifuro odu <oduibifuro@gmail.com>
2022-03-23 18:39:29 -0400

    updates

--- server/server.js
+++ server/server.js
@@ -54,7 +54,6 @@ app.use("/bookings", bookingsRoutes(db));
 app.use("/availabilities", availabilitiesRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
-// Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
