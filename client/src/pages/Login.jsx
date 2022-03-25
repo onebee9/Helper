e7042c1d24a0961a9e9618941b3c7ea980a5e909
mikiyoshi <mikiyoshi.kokura@gmail.com>
2022-03-25 13:12:02 -0400

    update signin link

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -20,7 +20,7 @@ import { authContext } from './../providers/AuthProvider';
 import { useContext } from 'react';
 import axios from 'axios';
 import qs from 'qs';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link as RouterLink } from 'react-router-dom';
 // import { useHistory } from 'react-router-dom';
 
 const theme = createTheme();
@@ -89,10 +89,18 @@ export default function Login() {
               gridTemplateColumns: 'repeat(2, 1fr)',
             }}
           >
-            <Link to="SignupService" style={{ textDecoration: 'none' }}>
+            <Link
+              to="/SignupService"
+              component={RouterLink}
+              style={{ textDecoration: 'none' }}
+            >
               <Button variant="contained">Signup Service</Button>
             </Link>
-            <Link to="SignupClient" style={{ textDecoration: 'none' }}>
+            <Link
+              to="/SignupClient"
+              component={RouterLink}
+              style={{ textDecoration: 'none' }}
+            >
               <Button variant="contained">Signup Client</Button>
             </Link>
           </Box>
