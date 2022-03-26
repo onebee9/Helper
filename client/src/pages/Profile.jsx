1ae025671ab035f9c6c45e739c6999182940a571
ibifuro odu <oduibifuro@gmail.com>
2022-03-25 23:33:38 -0400

    new components for dual profiles

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -26,8 +26,11 @@ import {
 import EditIcon from '@mui/icons-material/Edit';
 // import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import ClientProfile from "./ClientProfile"
+import ServiceProfile from "./ServiceProfile"
 
 const theme = createTheme();
+
 
 export default function Profile(props) {
   const [userStatus, setUserStatus] = useState({});
@@ -36,168 +39,17 @@ export default function Profile(props) {
     setUserStatus(JSON.parse(user));
   }, []);
 
+  console.log(userStatus);
+
   //show date in properformat
   const newDate = userStatus.data && new Date(userStatus.data.created_at);
   const completeDate = newDate && newDate.toDateString();
   const yearFinal = completeDate && completeDate.slice(4);
 
-  const provider =
-    userStatus.data && userStatus.data.isserviceprovider ? 'yes' : 'No';
+  const provider = userStatus.data && userStatus.data.isserviceprovider ? 'yes' : 'No';
+  const profileType =  provider === 'yes'? (<ServiceProfile user={userStatus}/>) : (<ClientProfile user={userStatus}/>);
 
   return (
-    <ThemeProvider theme={theme}>
-      <Navbar />
-      <main>
-        <Grid container>
-          <Grid item xs={4}>
-            <Container maxWidth="sm">
-              <Card sx={{ width: 1 }}>
-                <Box
-                  sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    p: 1,
-                    m: 1,
-                  }}
-                >
-                  <Avatar
-                    alt="Remy Sharp"
-                    src="https://htmlstream.com/preview/front-dashboard-v2.0/assets/img/160x160/img6.jpg"
-                    sx={{ width: 1 / 2, height: 1 / 2 }}
-                  />
-                </Box>
-                <CardContent
-                  sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                  }}
-                >
-                  <Typography variant="h5" color="text.secondary">
-                    {userStatus?.data?.first_name} {userStatus?.data?.last_name}
-                  </Typography>
-                </CardContent>
-
-                <nav aria-label="secondary mailbox folders">
-                  <List>
-                    <Link to="Profile" style={{ textDecoration: 'none' }}>
-                      <ListItem disablePadding>
-                        <ListItemButton>
-                          <ListItemText primary="Profile" />
-                        </ListItemButton>
-                      </ListItem>
-                    </Link>
-                    <Link to="Profile Edit" style={{ textDecoration: 'none' }}>
-                      <ListItem disablePadding>
-                        <ListItemButton>
-                          <ListItemText primary="Profile Edit" />
-                        </ListItemButton>
-                      </ListItem>
-                    </Link>
-                    <Link
-                      to="Profile Service"
-                      style={{ textDecoration: 'none' }}
-                    >
-                      <ListItem disablePadding>
-                        <ListItemButton>
-                          <ListItemText primary="Profile Servic" />
-                        </ListItemButton>
-                      </ListItem>
-                    </Link>
-                    <Link
-                      to="Profile Service Edit"
-                      style={{ textDecoration: 'none' }}
-                    >
-                      <ListItem disablePadding>
-                        <ListItemButton>
-                          <ListItemText primary="Profile Service Edit" />
-                        </ListItemButton>
-                      </ListItem>
-                    </Link>
-                  </List>
-                </nav>
-              </Card>
-            </Container>
-          </Grid>
-
-          <Grid item xs={8}>
-            <Container maxWidth="sm">
-              <TableContainer component={Paper}>
-                <Table sx={{ width: 1 }}>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell align="center" colSpan={3}>
-                        Profile
-                      </TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    <TableRow
-                      sx={{
-                        '&:last-child td, &:last-child th': { border: 0 },
-                      }}
-                    >
-                      <TableCell component="th" scope="row">
-                        Name : {userStatus?.data?.first_name}{' '}
-                        {userStatus?.data?.last_name}
-                      </TableCell>
-                      <TableCell></TableCell>
-                      <TableCell align="right"></TableCell>
-                    </TableRow>
-                    <TableRow
-                      sx={{
-                        '&:last-child td, &:last-child th': { border: 0 },
-                      }}
-                    >
-                      <TableCell component="th" scope="row">
-                        Email : {userStatus?.data?.email}
-                      </TableCell>
-                      <TableCell></TableCell>
-                      <TableCell align="right"></TableCell>
-                    </TableRow>
-                    <TableRow
-                      sx={{
-                        '&:last-child td, &:last-child th': { border: 0 },
-                      }}
-                    >
-                      <TableCell component="th" scope="row">
-                        {' '}
-                        Member from : {yearFinal}{' '}
-                      </TableCell>
-                      <TableCell></TableCell>
-                      <TableCell align="right"></TableCell>
-                    </TableRow>
-                    <TableRow
-                      sx={{
-                        '&:last-child td, &:last-child th': { border: 0 },
-                      }}
-                    >
-                      <TableCell component="th" scope="row">
-                        Is Service Prvide : {provider}{' '}
-                      </TableCell>
-                      <TableCell></TableCell>
-                      <TableCell align="right"></TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </Container>
-          </Grid>
-        </Grid>
-      </main>
-      {/* Footer */}
-      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom></Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          We Help You!
-        </Typography>
-        {/* <Copyright /> */}
-      </Box>
-      {/* End footer */}
-    </ThemeProvider>
-  );
+    <>{profileType}</>
+);
 }
