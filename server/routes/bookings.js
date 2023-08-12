8ec8c76e5b20581868e2d5ed8439f304dbfd4845
Ibifuro Odu <ibifuro@enzyme.com>
2023-08-11 21:43:45 -0400

    Added error handling for Endpoints

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -1,16 +1,14 @@
 /*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into api/users,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ * All routes for bookings are defined here
+and are mounted onto /booking in server.js
  */
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
   //create service booking
-  router.post('/new', (req, res) => {
+  router.post("/", async (req, res) => {
     const queryString = `INSERT INTO service_bookings(
       users_id,
       title,
@@ -28,50 +26,46 @@ module.exports = (db) => {
       req.body.end,
     ];
 
-    db.query(queryString, values)
-      .then((data) => {
-        const newBooking = data.rows;
-        res.json({ newBooking });
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
+    try {
+      const data = await db.query(query, values);
+      const booking = data.rows;
+      if (booking) {
+        res.status(200).json({ booking });
+      } else {
+        res.status(400).json({ error: "Booking creation failed" });
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
   });
 
-  router.put('/ratings', async (req, res) => {
+  //PUT /api/:id
+  router.put("/:id", async (req, res) => {
+    const { title, rating, status } = req.params;
+    const query = ` UPDATE service_bookings  SET title = $1, rating = $2, status = $3 WHERE id = $4::integer`;
+    const values = [title, rating, status, Number(req.params.id)];
 
+    try {
+      await db.query(query, values);
+      res.json("successfully updated");
+    } catch (err) {
+      res.status(400).json({ error: "Update failed." });
+    }
+  });
+
+  router.put("/ratings", async (req, res) => {
     const { bookingId, rating } = req.body;
     const params = [parseInt(rating), parseInt(bookingId, 10)];
     const query = "UPDATE service_bookings SET rating = $1 where id = $2";
 
     db.query(query, params)
       .then((data) => {
-        res.json('successfully updated')
-      })
-      .catch(error => console.log(error));
-  });
-
-
-  //update service booking
-  router.put('/update/:id', (req, res) => {
-    const { title, rating, status } = req.params;
-
-    db.query(
-      `
-  UPDATE service_bookings 
-  SET title = $1, rating = $2, status = $3
-  WHERE id = $4::integer
-`,
-      [title, rating, status, Number(req.params.id)]
-    )
-      .then((data) => {
-        res.json('successfully updated');
+        res.json("successfully updated");
       })
       .catch((error) => console.log(error));
   });
 
-
-  router.get('/ratings/:id', (req, res) => {
+  router.get("/ratings/:id", (req, res) => {
     const bookingId = req.params.id;
     const queryString = `SELECT rating FROM service_bookings WHERE id = $1`;
 
@@ -79,63 +73,6 @@ module.exports = (db) => {
       .then((data) => {
         const rating = data.rows;
         res.json(rating[0]);
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
-  });
-
-  //retrive service booking
-  router.get('/provider/:id', (req, res) => {
-    const serviceID = req.params.id;
-
-    const queryString = `SELECT
-    S1.id as booking_id,  S1.rating, S1.status, S1.created_at, S1. st_date as start_time, S1.end_date as end_time,
-    U1.id as client_id, U1.first_name as client_first_name, U1.last_name as client_last_name, U1.email as client_email,
-    S2.id as services_id, S2.title, S2.category, S2.description, S2.fee,
-    U2.first_name as provider_first_name, U2.email as provider_email_address,U2.id as provider_id,
-    CONCAT(L.num, ' ', L.street,', ', L.Metropolitan, ', ', L.city, ',', L.postal_code) as address
-    FROM
-    service_bookings S1
-    LEFT JOIN users U1 ON S1.users_id = U1.id
-    LEFT JOIN services S2 ON S1.services_id = S2.id
-    LEFT JOIN users U2 ON S2.user_id = U2.id
-    LEFT JOIN locations L ON L.user_id = U1.id
-    WHERE U2.id = $1`;
-
-    db.query(queryString, [serviceID])
-      .then((data) => {
-        const serviceBookings = data.rows;
-        res.json(serviceBookings);
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
-  });
-
-  //retrieve client bookings
-  router.get('/:id', (req, res) => {
-    const userID = req.params.id;
-
-    //filter by date as well when you have more dates.
-    const queryString = `SELECT
-    S1.id as booking_id,  S1.rating, S1.status, S1.created_at, S1. st_date as start_time, S1.end_date as end_time,
-    U1.id as client_id, U1.first_name as client_first_name, U1.last_name as client_last_name, U1.email as client_email,
-    S2.id as services_id, S2.title, S2.category, S2.description, S2.fee,
-    U2.first_name as provider_first_name, U2.email as provider_email_address,
-    CONCAT(L.num, ' ', L.street,', ', L.Metropolitan, ', ', L.city, ',', L.postal_code) as address
-    FROM
-    service_bookings S1
-    LEFT JOIN users U1 ON S1.users_id = U1.id
-    LEFT JOIN services S2 ON S1.services_id = S2.id
-    LEFT JOIN users U2 ON S2.user_id = U2.id
-    LEFT JOIN locations L ON L.user_id = U1.id
-    WHERE U1.id = $1`;
-
-    db.query(queryString, [userID])
-      .then((data) => {
-        const clientBookings = data.rows;
-        res.json(clientBookings);
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
--- server/routes/services.js
+++ server/routes/services.js
@@ -1,57 +1,127 @@
 /*
- * All routes for Widgets are defined here
- * Since this file is loaded in server.js into api/widgets,
- *   these routes are mounted onto /widgets
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ * All routes for services are defined here
+and are mounted onto /services in server.js
  */
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
+  //GET /api/services
   //Get all services provided on the platform, for use outside the search page if needed.
-  router.get('/', (req, res) => {
-    let query = `SELECT * FROM services WHERE services.id IS NOT NULL ;`;
-    db.query(query)
-      .then((data) => {
-        const services = data.rows;
-        res.json({ services });
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
+  router.get("/", async (req, res) => {
+    try {
+      const query = `SELECT * FROM services WHERE services.id IS NOT NULL ;`;
+      const allServiceData = await db.query(query);
+      const services = allServiceData.rows;
+
+      if (services) {
+        res.status(200).json({ services });
+      } else {
+        res.status(404).json({ error: "No services found" });
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
   });
 
-  //create new services
-  router.post('/new', (req, res) => {
-    const userID = req.session.user_id;
-    const queryString = `INSERT INTO services(
-    user_id,
-    title,
-    description,
-    category,
-    fee) VALUES ($1,$2,$3,$4,$5) RETURNING *;`;
+  //POST /api/services
+  router.post("/", async (req, res) => {
+    const query = `INSERT INTO services( user_id, title, description, category, fee) 
+      VALUES ($1,$2,$3,$4,$5) RETURNING *;`;
 
     const values = [
-      userID,
+      req.session.user_id,
       req.body.title,
       req.body.description,
       req.body.category,
       req.body.fee,
     ];
 
-    db.query(queryString, values)
-      .then((data) => {
-        const service = data.rows;
-        res.json({ service });
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
+    try {
+      const data = await db.query(query, values);
+      const service = data.rows;
+      if (service) {
+        res.status(200).json({ service });
+      } else {
+        res.status(404).json({ error: "Service creation failed" });
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
   });
 
-  //update service
-  router.put('/update/:id', (req, res) => {
+  router.get("/search", async (req, res) => {
+    const queryParams = [];
+    const { keyword, category, price, location } = req.query;
+
+    let query = `SELECT services.*, users.first_name
+    FROM services
+    JOIN Users ON users.id = services.user_id
+    JOIN locations ON users.id = locations.user_id
+    WHERE services.id IS NOT NULL `;
+
+    //validate that search params exist and then add on to the query
+    if (!keyword == "") {
+      queryParams.push(`%${keyword}%`);
+      query += `AND services.description ILIKE $${queryParams.length} `;
+    }
+
+    if (!category == "") {
+      queryParams.push(category);
+      query += `AND services.category = $${queryParams.length} `;
+    }
+
+    if (!price == "") {
+      let priceToNumber = parseInt(price);
+      queryParams.push(priceToNumber);
+      query += `AND services.fee <= $${queryParams.length} `;
+    }
+
+    if (!location == "") {
+      queryParams.push(location);
+      query += `AND locations.Metropolitan = $${queryParams.length} `;
+    }
+
+    query += `;`;
+
+    try {
+      const data = await db.query(query, queryParams);
+      const searchResults = data.rows;
+
+      if (searchResults) {
+        res.status(200).json(searchResults);
+      } else {
+        res.status(404).json({ error: "No data found" });
+      }
+    } catch (err) {
+      res.status(500).json({
+        error: err.message,
+        queryData: queryString,
+      });
+    }
+  });
+
+  //GET /api/services/:id
+  router.get("/:id", async (req, res) => {
+    const serviceId = req.params.id;
+    const query = `SELECT * FROM services where id = $1;`;
+
+    try {
+      const data = await db.query(query, [serviceId]);
+      const serviceData = data.rows;
+      if (serviceData) {
+        res.status(200).json(serviceData[0]);
+      } else {
+        res.status(404).json({ error: "Not found" });
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+
+  // PUT /api/services/:id
+  router.put("/:id", async (req, res) => {
     const queryParams = [];
     const id = req.params.id;
     const { title, category, description, fee } = req.body;
@@ -59,173 +129,65 @@ module.exports = (db) => {
     let values = [];
 
     //Check what the user would like to update and add on to the query
-    if (!title == '') {
+    if (!title == "") {
       queryParams.push(title);
-      columns.push('title');
+      columns.push("title");
       values.push(title);
       //queryString += `title = $${multipleOrNot(title)} `;
     }
 
-    if (!category == '') {
+    if (!category == "") {
       queryParams.push(category);
-      columns.push('category');
+      columns.push("category");
       values.push(category);
-   
     }
 
-    if (!fee == '') {
+    if (!fee == "") {
       let feeToNumber = parseInt(fee);
       queryParams.push(feeToNumber);
-      columns.push('fee');
+      columns.push("fee");
       values.push(feeToNumber);
     }
 
-    if (!description == '') {
+    if (!description == "") {
       queryParams.push(description);
-      columns.push('description');
+      columns.push("description");
       values.push(description);
     }
 
     let params = [id];
-    let query = 'UPDATE users SET ';
+    let query = "UPDATE users SET ";
     for (let i = 0; i < columns.length; i++) {
-
-      query += query + ' ' + columns[i] + ' = ' + params.length + 1;
+      query += query + " " + columns[i] + " = " + params.length + 1;
       params.push(queryParams[columns[i]]);
     }
 
-    query = query.substring(0, query.length - 1) + ' WHERE id = $1';
+    query = query.substring(0, query.length - 1) + " WHERE id = $1";
 
-
-    db.query(query, params)
-      .then((data) => {
-        res.json('successfully updated');
-      })
-      .catch((error) => console.log(error));
+    try {
+      await db.query(query, [...params, ...queryParams]);
+      res.status(200).json({ message: "Service successfully updated" });
+    } catch (err) {
+      res
+        .status(500)
+        .json({ error: "Failed to update service", details: err.message });
+    }
   });
-
-  //Return all services from database, uses filters when provided
-  router.get('/search', (req, res) => {
-    const queryParams = [];
-    const { keyword, category, price, location } = req.query;
-    console.log(req.query);
-
-    let queryString = `SELECT services.*, users.first_name
-    FROM services
-    JOIN Users ON users.id = services.user_id
-    JOIN locations ON users.id = locations.user_id
-    WHERE services.id IS NOT NULL `;
-
-    //validate that search params exist and then add on to the query
-    if (!keyword == '') {
-      queryParams.push(`%${keyword}%`);
-      queryString += `AND services.description ILIKE $${queryParams.length} `;
-    }
-
-    if (!category == '') {
-      queryParams.push(category);
-      queryString += `AND services.category = $${queryParams.length} `;
-    }
-
-    if (!price == '') {
-      let priceToNumber = parseInt(price);
-      queryParams.push(priceToNumber);
-      queryString += `AND services.fee <= $${queryParams.length} `;
-    }
-
-    if (!location == '') {
-      queryParams.push(location);
-      queryString += `AND locations.Metropolitan = $${queryParams.length} `;
-    }
-
-    queryString += `;`;
-
-    console.log(queryString);
-    console.log(queryParams);
-
-    db.query(queryString, queryParams)
-      .then((data) => {
-        if (data) {
-          const searchResults = data.rows;
-
-          res.json(searchResults);
-          return;
-        }
-        res.json('No matching search results');
-      })
-      .catch((err) => {
-        res.status(500).json({
-          error: err.message,
-          queryData: queryString,
-        });
-      });
-  });
-
-  router.get('/averageRating/:id', (req, res) => {
-    const serviceId = req.params.id;
-    const queryString = `SELECT avg(rating) FROM service_bookings WHERE id = $1`;
-
-    db.query(queryString, [serviceId])
-      .then((data) => {
-        const rating = data.rows;
-        res.json(rating[0]);
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
-  });
-
 
   //Delete a service
-  router.delete('/remove/:id', (req, res) => {
+  router.delete("/:id", async (req, res) => {
     const serviceId = Number(req.params.id);
-    const queryString = `DELETE FROM services WHERE id = $1;`;
-
-    db.query(queryString, [serviceId])
-      .then((data) => {
-        if (data.rows.length > 0) {
-          res.status(200).json('success');
-        } else {
-          res.status(500).send('Account not found.');
-        }
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
-  });
-
-  //Get all services a provider offers
-  router.get('/provider/:id', (req, res) => {
-    const serviceProviderID = req.params.id;
-    let query = `SELECT * FROM services where user_id = $1;`;
-
-    db.query(query, [serviceProviderID])
-      .then((data) => {
-        const services = data.rows;
-        res.json(services);
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
-  });
-
-  //Get all data for a specific service
-  router.get('/:id', (req, res) => {
-    const serviceProviderID = req.params.id;
-    let query = `SELECT * FROM services where id = $1;`;
-
-    db.query(query, [serviceProviderID])
-      .then((data) => {
-        const services = data.rows;
-        if (services.length == 0) {
-          res.status(404).json({ error: 'Not Found' });
-          return;
-        }
-        res.json(services[0]);
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
+    const query = `DELETE FROM services WHERE id = $1;`;
+    try {
+      const data = await db.query(query, [serviceId]);
+      if (data.rows.length > 0) {
+        res.status(200).json("success");
+      } else {
+        res.status(404).send("failed.");
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
   });
 
   return router;
